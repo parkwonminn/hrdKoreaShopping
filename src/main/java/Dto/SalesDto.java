35835fcb24befcdc4ba67f6e3c013d82d2ad61c8
jungwoogyun <jwg8910@naver.com>
2022-12-27 10:47:07 +0900

    02

--- src/main/java/Dto/SalesDto.java
+++ src/main/java/Dto/SalesDto.java
@@ -9,6 +9,43 @@ public class SalesDto {
 	//Default 생성자
 	public SalesDto() {}
 	//getter and setter 
+
+	public String getCustno() {
+		return custno;
+	}
+
+	public void setCustno(String custno) {
+		this.custno = custno;
+	}
+
+	public String getCustname() {
+		return custname;
+	}
+
+	public void setCustname(String custname) {
+		this.custname = custname;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public String getSales() {
+		return sales;
+	}
+
+	public void setSales(String sales) {
+		this.sales = sales;
+	}
+
+	@Override
+	public String toString() {
+		return "SalesDto [custno=" + custno + ", custname=" + custname + ", grade=" + grade + ", sales=" + sales + "]";
+	}
 	
 	//toString
 	
